bf026aff814ce9e8023cbf919d670110060c2727
Ayan Das <ayandas9531@gmail.com>
2023-01-05 23:06:51 +0530

    error page template ;

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+import errorImage from '../assets/page-not-found 1.png';
+import '../assets/styles/error.css'
+
 export const Error = () => {
     return (
         <div className="error-container">
-            This is an error
-       </div>
+            <h1>Page Not Found</h1>
+            <div className="notFoundImage">
+                <img src={errorImage} alt="Error" />
+            </div>
+            <p>You might want to go to <strong><Link to={'/'}>Home Page</Link></strong></p>
+        </div>
     )
 }
