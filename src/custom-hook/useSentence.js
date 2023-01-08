cc5f5b3a4090da048d8b5df8bf6cd43246720baa
Ayan Das <ayandas9531@gmail.com>
2023-01-08 09:18:42 +0530

    returns string from API

--- src/custom-hook/useSentence.js
+++ src/custom-hook/useSentence.js
@@ -1,26 +1,13 @@
-import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useQuery } from 'react-query';
 
 function useAPI() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const { data, error, isLoading } = useQuery(["sentence"], () => {
+    return axios.get("http://metaphorpsum.com/paragraphs/5").then(res => res.data);
+  })
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://metaphorpsum.com/paragraphs/5');
-        setData(response.data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+  return { data, error, isLoading };
 
-  return { data, error, loading, setData};
 }
 
 export default useAPI;
