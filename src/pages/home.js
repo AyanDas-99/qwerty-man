9eb5d9d0669af4cfdac5c7efc9e00e7d0afa618a
Ayan Das <ayandas9531@gmail.com>
2023-01-08 19:58:29 +0530

    checking; highlighting; autoDeleting - complete

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,5 @@
 import arrow from '../assets/arrow.png'
 import retryImg from '../assets/retry.png'
-import '../assets/styles/keyboard.css'
 import '../assets/styles/home.css'
 import useAPI from '../custom-hook/useSentence'
 import { useEffect, useState } from 'react'
@@ -8,28 +7,39 @@ import { useEffect, useState } from 'react'
 export const Home = () => {
 
     const { data, error, isLoading } = useAPI();
-    const [inputText, setInputText] = useState("");
-    const [correctWords, setCorrectWords] = useState(0);
-    const [text, setText] = useState("");
+    const [correctLetters, setCorrectLetters] = useState(0);
+    const [wrongLetters, setLetters] = useState(0);
+    const [text, setText] = useState([]);
 
     useEffect(() => {
-        setText(String(data));
+        setText([...String(data)].filter(e => e !== '\n'));
     }, [data])
+    console.log(text)
 
     const typed = (e) => {
-        const allLetters = Array.from(document.getElementsByClassName('letter'));
-        (allLetters).map(e => e.classList.remove('highLight'));
-        if (e.target.value === text.substring(0, e.target.value.length)) {
-            if(e.target.value.charAt(e.target.value.length-1) === ' ') {
-                setText(text.substring(e.target.value.length));
-                e.target.value = '';
-                console.log('correct word')
+
+        // spans array
+        const letters = Array.from(document.getElementsByClassName('letter'));
+
+
+        // removing previous white color
+        letters.map(e => e.classList.remove('highLight', 'wrongLetter'))
+
+        console.log(letters)
+        // Checking if correct
+        for (let i = 0; i < e.target.value.length; i++) {
+            if (e.target.value.charAt(i) === letters[i].textContent) {
+                letters[i].classList.add('highLight');
             }
-            for (let i = 0; i < e.target.value.length; i++) {
-                allLetters[i].classList.add('highLight');
-                console.log(allLetters[i])
+            else {
+                letters[i].classList.add('wrongLetter');
             }
 
+            if (letters[i].textContent === ' ') {
+                setText(text.splice(e.target.value.length));
+                e.target.value = '';
+                letters.map(e => e.classList.remove('highLight', 'wrongLetter'))
+            }
         }
     }
 
@@ -40,13 +50,17 @@ export const Home = () => {
                 <p>The All-round Typing Practice Site.</p>
                 <img src={arrow} alt='arrow' />
             </div>
+
             <div className='test-section'>
                 <div className='sentence'>
                     <Words sentence={text} />
                 </div>
+
                 <div className='write'>
                     <input type={"text"} onChange={(e) => typed(e)} />
+                    <div>{ }</div>
                 </div>
+
                 <div className='retryBtn'>
                     <img src={retryImg} alt="retry btn" />
                 </div>
@@ -56,11 +70,10 @@ export const Home = () => {
 }
 
 const Words = (props) => {
-    const data = props.sentence;
-    console.log(data)
+    const data = props.sentence
     return (
         <p>
-            {[...data].map((e, key) => <span key={key} className='letter'>{e}</span>)}
+            {data.map((e, key) => <span key={key} className='letter'>{e}</span>)}
         </p>
     )
 }
