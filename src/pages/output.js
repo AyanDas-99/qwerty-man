b6d66d9536b78e0b5f2fdb7e1b863e484033244c
Ayan Das <ayandas9531@gmail.com>
2023-01-09 19:50:05 +0530

    calculates typing result

--- src/pages/output.js
+++ src/pages/output.js
@@ -1,9 +1,13 @@
 import '../assets/styles/output.css'
-export const Output = () => {
+export const Output = (props) => {
+    const netSpeed = props.correctWords / props.time;
+    const grossSpeed = props.totalWords / props.time;
+    const accuracy = netSpeed * 100 / grossSpeed;
+
     return (
         <div className='output-container'>
             <div className="output">
-                <p className='speed'>123</p>
+                <p className='speed'>{netSpeed}</p>
                 <p>WPM</p>
             </div>
         </div>
