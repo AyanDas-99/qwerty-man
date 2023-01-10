223d8f12137a615bcc52427724966176b74852a8
Ayan Das <ayandas9531@gmail.com>
2023-01-10 23:11:02 +0530

    exported refetch function

--- src/custom-hook/useSentence.js
+++ src/custom-hook/useSentence.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 function useAPI() {
-  const { data, error, isLoading } = useQuery(["sentence"], () => {
+  const { data, error, isLoading, refetch } = useQuery(["sentence"], () => {
     return axios.get("http://metaphorpsum.com/paragraphs/5").then(res => res.data);
   })
 
-  return { data, error, isLoading };
+  return { data, error, isLoading, refetch };
 
 }
 
