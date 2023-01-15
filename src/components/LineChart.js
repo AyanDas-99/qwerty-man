2b02c233617d8b704c0ce0f888fffc210e8e3906
Ayan Das <ayandas9531@gmail.com>
2023-01-15 23:13:02 +0530

    Chart working

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,32 +1,18 @@
 import { Line } from 'react-chartjs-2';
 import { } from "chart.js/auto"
-import { useState, useEffect } from 'react';
 
 const LineChart = ({ timeline }) => {
-    const [chartData, setChartdata] = useState(null)
-    const [userdata, setUserdata] = useState([]);
+    const timelineData = timeline.sort((a, b) => new Date(a.date) - new Date(b.date));
+    const chartData = {
+        labels: timelineData.map(data => data.date),
+        datasets: [{
+            label: "Speed",
+            data: timelineData.map(data => data.speed),
+        }]
+    }
 
-    console.log(userdata)
-    useEffect(() => {
-        if (timeline.length !== 0) {
-            setUserdata(timeline.sort((a, b) => new Date(a.date) - new Date(b.date)));
-            setChartdata({
-                labels: userdata.map(data => data.date),
-                datasets: [{
-                    label: "speed",
-                    data: userdata.map(data => data.speed),
-                }]
-            })
-        }
-    }, [timeline])
-
-
-    return <div style={{ width: 500 }}>
-        {chartData &&
-            <Line data={chartData} />
-        }
-    </div>
-
+    return <Line data={chartData} />
 }
+
 
 export default LineChart;
