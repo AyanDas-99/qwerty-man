27c20091d01629e43020710953bfc1d3286f97f4
Ayan Das <ayandas9531@gmail.com>
2023-01-15 12:22:16 +0530

    now takes prop as timeline data, instead of chartData

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,11 +1,28 @@
-import { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { } from "chart.js/auto"
+import { useState, useEffect } from 'react';
 
-const LineChart = ({ chartData }) => {
+const LineChart = ({ timeline }) => {
+    const [chartData, setChartdata] = useState(null)
+    useEffect(() => {
+        if (timeline.length !== 0) {
+            setChartdata({
+                labels: timeline.map(data => data.date.split('-')[0]),
+                datasets: [{
+                    label: "speed",
+                    data: timeline.map(data => data.speed),
+                }]
+            })
+        }
+    }, [timeline])
+
+
     return <div style={{ width: 500 }}>
-        <Line data={chartData} />
+        {chartData &&
+            <Line data={chartData} />
+        }
     </div>
+
 }
 
 export default LineChart;
