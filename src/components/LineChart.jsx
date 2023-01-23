0b794c0f9d021c194c15dae0f1af8d77cd1a8d61
Ayan Das <ayandas9531@gmail.com>
2023-01-23 22:52:02 +0530

    chart date sorted

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,23 +1,34 @@
-import { Line } from 'react-chartjs-2';
-import { } from "chart.js/auto"
+import { Line } from "react-chartjs-2";
+import {} from "chart.js/auto";
 
 const LineChart = ({ timeline }) => {
-    console.log(timeline)
-    const timelineData = timeline.sort((a, b) => new Date(a.date) - new Date(b.date));
-    console.log(timelineData)
-    const chartData = {
-        labels: timelineData.map(data => data.date),
-        datasets: [{
-            label: "Speed",
-            data: timelineData.map(data => data.speed),
-        }]
-    }
-    const options = {
-        color: 'white'
-    }
+  const timelineData = timeline.sort(
+    (a, b) =>
+      new Date(
+        `${a.date.split("-")[1]}-${a.date.split("-")[0]}-${
+          a.date.split("-")[2]
+        }`
+      ) -
+      new Date(
+        `${b.date.split("-")[1]}-${b.date.split("-")[0]}-${
+          b.date.split("-")[2]
+        }`
+      )
+  );
+  const chartData = {
+    labels: timelineData.map((data) => data.date),
+    datasets: [
+      {
+        label: "Speed",
+        data: timelineData.map((data) => data.speed),
+      },
+    ],
+  };
+  const options = {
+    color: "white",
+  };
 
-    return <Line data={chartData} className="chart" options={options}/>
-}
-
+  return <Line data={chartData} className="chart" options={options} />;
+};
 
 export default LineChart;
--- src/components/accuracyChart.jsx
+++ src/components/accuracyChart.jsx
@@ -1,21 +1,35 @@
-import { Line } from 'react-chartjs-2';
-import { } from "chart.js/auto"
+import { Line } from "react-chartjs-2";
+import {} from "chart.js/auto";
 
 const AccuracyChart = ({ timeline }) => {
-    const timelineData = timeline.sort((a, b) => new Date(a.date) - new Date(b.date));
-    const chartData = {
-        labels: timelineData.map(data => data.date),
-        datasets: [{
-            label: "Accuracy",
-            data: timelineData.map(data => data.accuracy),
-        }]
-    }
+  const timelineData = timeline.sort(
+    (a, b) =>
+      new Date(
+        `${a.date.split("-")[1]}-${a.date.split("-")[0]}-${
+          a.date.split("-")[2]
+        }`
+      ) -
+      new Date(
+        `${b.date.split("-")[1]}-${b.date.split("-")[0]}-${
+          b.date.split("-")[2]
+        }`
+      )
+  );
+  timelineData.map((e) => console.log(e.date));
+  const chartData = {
+    labels: timelineData.map((data) => data.date),
+    datasets: [
+      {
+        label: "Accuracy",
+        data: timelineData.map((data) => data.accuracy),
+      },
+    ],
+  };
 
-    const options = {
-        color: 'white',
-    }
-    return <Line data={chartData} className="chart" options={options} />
-}
-
+  const options = {
+    color: "white",
+  };
+  return <Line data={chartData} className="chart" options={options} />;
+};
 
 export default AccuracyChart;
