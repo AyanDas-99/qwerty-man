cf8df36bbe2ba373b88b9815660580c3300e186d
Ayan Das <ayandas9531@gmail.com>
2023-01-26 00:29:57 +0530

    added timeline, date selection working

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -1,9 +1,150 @@
-import '../assets/styles/timeline.css'
+import "../assets/styles/timeline.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../config/firebase";
+import { useState } from "react";
+import AccuracyChart from "../components/accuracyChart";
+import LineChart from "../components/LineChart";
+import useGetTimeline from "../custom-hook/useGetTimeline";
 
 export const Timeline = () => {
-    return (
-        <div className="timeline-container">
-            Timeline
-        </div>
-    )
-}
+  const [user] = useAuthState(auth);
+  const { timeline } = useGetTimeline(user?.uid);
+
+  return (
+    <div className="timeline-container">
+      {/* Profile Section */}
+      <ProfileSection
+        dp={user?.photoURL}
+        username={user?.displayName}
+        email={user?.email}
+      />
+
+      {/* Speed and accuracy data section */}
+      {timeline && user && <DataSection timeline={timeline} />}
+      <HistoryData timeline={timeline} />
+    </div>
+  );
+};
+
+const ProfileSection = (props) => {
+  return (
+    <div className="profile">
+      <img src={props.dp} alt="display profile" />
+      <div className="user-details">
+        <h2>{props.username}</h2>
+        <h4>{props.email}</h4>
+      </div>
+    </div>
+  );
+};
+
+const DataSection = (props) => {
+  const [type, setType] = useState("SPEED");
+  // console.log(props.timeline)
+  const typeToggle = () => {
+    if (type === "SPEED") setType("ACCURACY");
+    else setType("SPEED");
+  };
+
+  return (
+    <div className="data_section">
+      <button onClick={typeToggle}>{type}</button>
+      <div className="data">
+        <LineChart timeline={props.timeline} />
+        <AccuracyChart timeline={props.timeline} />
+      </div>
+    </div>
+  );
+};
+
+const HistoryData = (props) => {
+  const timelineData = props.timeline.sort(
+    (a, b) =>
+      new Date(
+        `${a.date.split("-")[1]}-${a.date.split("-")[0]}-${
+          a.date.split("-")[2]
+        }`
+      ) -
+      new Date(
+        `${b.date.split("-")[1]}-${b.date.split("-")[0]}-${
+          b.date.split("-")[2]
+        }`
+      )
+  );
+
+  const days = Array.from(
+    new Set(timelineData.map(({ date }) => date.split("-")[0]))
+  );
+  const months = Array.from(
+    new Set(timelineData.map(({ date }) => date.split("-")[1]))
+  );
+  const years = Array.from(
+    new Set(timelineData.map(({ date }) => date.split("-")[2]))
+  );
+
+  const [userDate, setUserDate] = useState({
+    day: days[0],
+    month: months[0],
+    year: years[0],
+  });
+
+  return (
+    <div className="history-data">
+      <h3>HISTORY</h3>
+      <div className="date-selectors">
+        <label for="day">Day</label>
+        <select
+          id="day"
+          name="day"
+          onChange={(e) => {
+            setUserDate({ ...userDate, day: e.target.value });
+            console.log(userDate);
+          }}
+        >
+          {days.map((e) => (
+            <option value={e} key={e}>
+              {e}
+            </option>
+          ))}
+        </select>
+        <label for="month">Month</label>
+        <select
+          id="month"
+          name="month"
+          onChange={(e) => setUserDate({ ...userDate, month: e.target.value })}
+        >
+          {months.map((e) => (
+            <option value={e} key={e}>
+              {e}
+            </option>
+          ))}
+        </select>
+        <label for="year">Year</label>
+        <select
+          id="year"
+          name="year"
+          onChange={(e) => setUserDate({ ...userDate, year: e.target.value })}
+        >
+          {years.map((e) => (
+            <option value={e} key={e}>
+              {e}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="data-section">
+        {timelineData
+          .filter(
+            (e) =>
+              e.date === `${userDate.day}-${userDate.month}-${userDate.year}`
+          )
+          .map((e) => (
+            <p>
+              {e.speed} {e.date}
+            </p>
+          ))}
+      </div>
+    </div>
+  );
+};
