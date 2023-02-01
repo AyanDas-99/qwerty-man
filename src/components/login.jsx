276a189d7bab4a59e262a470514b4057978cf48b
Ayan Das <ayandas9531@gmail.com>
2023-02-02 01:45:24 +0530

    changed signin method from popup to redirect

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,30 +1,35 @@
-import { signInWithPopup, signOut } from 'firebase/auth'
-import { auth, provider } from '../config/firebase'
+import { signInWithRedirect, signOut } from "firebase/auth";
+import { collectionGroup } from "firebase/firestore";
+import { auth, provider } from "../config/firebase";
 
 export const Login = (props) => {
+  // Google signin function
+  const GSignIn = async () => {
+    console.log("Redirecting..")
+    await signInWithRedirect(auth, provider);
+  };
 
-    // Google signin function
-    const GSignIn = async () => {
-        await signInWithPopup(auth, provider);
-    }
+  // sign out funtion
+  const GSignOut = async () => {
+    await signOut(auth);
+  };
 
-    // sign out funtion
-    const GSignOut = async () => {
-        await signOut(auth);
-    }
-
-    return (
-        <div className="login-container no">
-            <div className="username no">
-                <div className="profile-pic no">
-                    <img src={props.dp} alt="profile" className='no' />
-                </div>
-                <p className='no'>{props.userName}</p>
-            </div>
-            <div className="actions no">
-                <div className="log blue no" onClick={GSignIn}>Switch Account</div>
-                <div className="log red no" onClick={GSignOut}>Logout</div>
-            </div>
+  return (
+    <div className="login-container no">
+      <div className="username no">
+        <div className="profile-pic no">
+          <img src={props.dp} alt="profile" className="no" />
         </div>
-    )
-}
+        <p className="no">{props.userName}</p>
+      </div>
+      <div className="actions no">
+        <div className="log blue no" onClick={GSignIn}>
+          Switch Account
+        </div>
+        <div className="log red no" onClick={GSignOut}>
+          Logout
+        </div>
+      </div>
+    </div>
+  );
+};
