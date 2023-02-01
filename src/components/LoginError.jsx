1b40e41d3039773f89ce81a903ce248417e55537
Ayan Das <ayandas9531@gmail.com>
2023-02-01 23:25:29 +0530

    new component for not logged in error

---
+++ src/components/LoginError.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+const LoginError = ({ pageName, setShowle }) => {
+  const [timer, setTimer] = useState(3);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((initial) => {
+        if (initial === 1) {
+          clearInterval(interval);
+          setShowle({ status: false, pageName: "" });
+        }
+        return initial - 1;
+      });
+    }, 1000);
+  }, []);
+
+  return (
+    <p
+      style={{
+        color: "white",
+        fontWeight: 600,
+        backgroundColor: "rgba(255, 0, 0, 0.359)",
+        padding: "10px",
+        margin: "10px",
+        borderRadius: "5px",
+        position: "absolute",
+        top: "0",
+        width: "100%",
+        padding: "2em",
+      }}
+    >
+      Please Sign In to access {pageName}{" "}
+      <span
+        style={{ border: "1px solid white", padding: "5px", margin: "0 2em" }}
+      >
+        {timer}
+      </span>
+    </p>
+  );
+};
+
+export default LoginError;
